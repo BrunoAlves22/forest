c5063193197962999797625dd929fcd9196af2a3
BrunoAlves22 <brunoalves220701@gmail.com>
2025-04-17 09:20:27 -0300

    fix animação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { AosInit } from "@/components/aos-init";
 
 export const metadata: Metadata = {
   title: "Forest",
@@ -12,12 +13,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth overflow-x-hidden">
       <body
-        className="bg-verde-800 font-sans py-3 antialiased"
+        className="bg-verde-800 font-sans py-3 antialiased overflow-x-hidden"
         cz-shortcut-listen="true"
       >
         {children}
+        <AosInit />
       </body>
     </html>
   );
